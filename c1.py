6a5d98c5d8ef42ba202ad89bc5a34c97d4503213
hung20071612-hue <hung20071612@gmail.com>
2026-05-22 09:58:06 +0700

    Delete c1.py

--- c1.py
+++
@@ -1,6 +0,0 @@
-price = float(input("Nhập đơn giá của sản phẩm: "))
-quantity = int(input("Nhập số lượng hàng mua: "))
-total_price = price * quantity
-if total_price >= 1_000_000:
-    total_price = total_price * 0.9
-print(f"Số tiền cuối cùng mà khánh cần thanh toán: {total_price} VND")
